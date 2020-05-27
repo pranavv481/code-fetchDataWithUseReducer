dbf049f57a1de064df3e895df30d365c924456c3
pranavv481 <pranavv481@gmail.com>
2020-05-27 13:28:27 +0530

    useReducer fetchdata

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component, useReducer } from 'react';
+import './App.css';
+import DataFetchingTwo from './components/DataFetchingTwo';
+// import DataFetchingOne from './components/DataFetchingOne';
+
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+      {/* <DataFetchingOne /> */}
+      <DataFetchingTwo />
+    </div>
+
+  );
+}
+
+export default App;
